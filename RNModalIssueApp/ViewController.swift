bbbed2ff005a76420a72dc78e5b2f6b133e1d8c5
CHOIMINSEOK <cms3718@gmail.com>
2025-01-21 22:50:49 +0900

    init

---
+++ RNModalIssueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RNModalIssueApp
+//
+//  Created by cms3718 on 1/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RNModalIssueAppTests/RNModalIssueAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RNModalIssueAppTests.swift
+//  RNModalIssueAppTests
+//
+//  Created by cms3718 on 1/21/25.
+//
+
+import Testing
+@testable import RNModalIssueApp
+
+struct RNModalIssueAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
