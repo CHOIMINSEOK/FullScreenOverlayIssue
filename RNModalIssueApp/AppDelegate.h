a075be4e62ee3f320b2d5f342cedfac67f8213a6
CHOIMINSEOK <cms3718@gmail.com>
2025-01-23 22:19:46 +0900

    fix build error

---
+++ RNModalIssueApp/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  RNModalIssueApp
+//
+//  Created by cms3718 on 1/23/25.
+//
+
+#import <RCTAppDelegate.h>
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : RCTAppDelegate
+
+@end
--- RNModalIssueApp/ViewController.swift
+++ RNModalIssueApp/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import React_RCTAppDelegate
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let factory = (RCTSharedApplication()?.delegate as? RCTAppDelegate)?.rootViewFactory
+        self.view = factory?.view(withModuleName: "HelloWorld")
     }
 
 
