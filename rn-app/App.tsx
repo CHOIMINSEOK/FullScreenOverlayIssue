1ce313028f9e0a0d331e7e01cb6bc4494dac3b2d
CHOIMINSEOK <cms3718@gmail.com>
2025-01-24 10:28:34 +0900

    add react-navigation

--- rn-app/App.tsx
+++ rn-app/App.tsx
@@ -1,56 +1,59 @@
 import {
+    Button,
     SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
+    StyleSheet, Text,
+    useColorScheme, View,
 } from 'react-native';
 
 import {
     Colors,
-    DebugInstructions,
-    Header,
-    ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer, useNavigation} from "@react-navigation/native";
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    const handleClick = () => {
+        navigation.navigate('Profile')
+    }
+
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Home Screen</Text>
+            <Button title={'Click'} onPress={handleClick}/>
+        </View>
+    )
+}
+
+const ProfileScreen = () => {
+    return (
+        <Text>
+            ProfileStack
+        </Text>
+    )
+}
 
 function App(): JSX.Element {
     const isDarkMode = useColorScheme() === 'dark';
 
-    const backgroundStyle = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    };
 
     return (
-        <SafeAreaView style={backgroundStyle}>
-            <StatusBar
-                barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-                backgroundColor={backgroundStyle.backgroundColor}
-            />
-            <ScrollView
-                contentInsetAdjustmentBehavior="automatic"
-                style={backgroundStyle}>
-                <Header />
-                <View
-                    style={{
-                        backgroundColor: isDarkMode
-                            ? Colors.black
-                            : Colors.white,
-                        padding: 24,
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="Home" screenOptions={{
+                    headerShown: false,
+                }}>
+                    <Stack.Screen name="Home" component={HomeScreen} />
+                    <Stack.Group screenOptions={{
+                        presentation: 'modal'
                     }}>
-                    <Text style={styles.title}>Step One</Text>
-                    <Text>
-                        Edit <Text style={styles.bold}>App.tsx</Text> to
-                        change this screen and see your edits.
-                    </Text>
-                    <Text style={styles.title}>See your changes</Text>
-                    <ReloadInstructions />
-                    <Text style={styles.title}>Debug</Text>
-                    <DebugInstructions />
-                </View>
-            </ScrollView>
-        </SafeAreaView>
+                        <Stack.Screen name="Profile" component={ProfileScreen} />
+                    </Stack.Group>
+                </Stack.Navigator>
+            </NavigationContainer>
     );
 }
 
